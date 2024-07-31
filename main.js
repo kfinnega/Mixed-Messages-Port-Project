23b02884abb017559d2d4a8809fd6450e949df1b
Kieran Finnegan <kierantfinnegan@gmail.com>
2024-07-31 15:18:51 -0400

    starting to work on the project

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello, Kieran!")
+const { open } = require('node.fs/promises');
+var quotes = [];
+
+
+function readQuotes() {
+  
+}
+
+// The method that is going to print the qoute to the console
+function printQuote(quote) {
+   console.log(quote);
+}
+
+printQuote('js is still working')
+
+
