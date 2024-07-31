e9f8648609b053ab491a10eb52d76cf1e14eaf3a
Kieran Finnegan <kierantfinnegan@gmail.com>
2024-07-31 15:36:34 -0400

    created file to hold the quotes and a method in main to read them line by line

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-const { open } = require('node.fs/promises');
 var quotes = [];
 
+const fs = require('fs');
+const readline = require('readline');
 
-function readQuotes() {
-  
+async function readQuotes() {
+  const fileStream = fs.createReadStream('quoteFile.txt');
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  for await (const line of rl) {
+    console.log(`line from file: ${line}`);
+  }
 }
 
 // The method that is going to print the qoute to the console
@@ -11,6 +19,6 @@ function printQuote(quote) {
    console.log(quote);
 }
 
-printQuote('js is still working')
+readQuotes();
 
 
